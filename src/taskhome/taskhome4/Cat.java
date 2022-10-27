259aaa92a722ef1ed9d6b538be0966dd2b30c4c4
IvanTereshcheko22 <glorex22@mail.ru>
2022-10-28 00:11:48 +0300

    1

--- src/taskhome/taskhome4/Cat.java
+++ src/taskhome/taskhome4/Cat.java
@@ -1,45 +1,66 @@
 package taskhome.taskhome4;
-
-import javax.naming.Name;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.random.RandomGenerator;
-import java.util.random.RandomGeneratorFactory;
-
 public class Cat {
-    static String name = "Barsik";
-    static int age = 5;
-    static int numberCat = 0;
+     final private static String name = "Barsik";
+     final private static int age = 5;
+    private static String cat_name;
+    private static int cat_age;
+
+    public Cat(int age, String name) {
+        cat_age = age;
+        cat_name = name;
+    }
 
     public static void firstCycle() {
-        while (numberCat < 10) {
+        int cat_number = 1;
+        System.out.println("\nFirst cycle \n");
+        while (cat_number <= 10) {
+            System.out.println("Cat number " + cat_number + ": " + " Age is " + age + "; Name is " + name);
+            cat_number++;
+        }
+    }
+
+    public static void secondCycle() {
+        System.out.println("\nSecond cycle \n");
+        for (int i = 1; i <= 10; i++) {
+            setAge(Main.randomAge());
+            setName(Main.randomName());
+            System.out.println("Cat number " + i + ": Age is " + getAge() + "; Name is " + getName());
+        }
+    }
+    public static String getName() {
+        return cat_name;
+    }
+    public static void setName(String cat_name) {
+        Cat.cat_name = cat_name;
+    }
+    public static int getAge() {
+        return cat_age;
+    }
+    public static void setAge(int cat_age) {
+        Cat.cat_age = cat_age;
+    }
+
+    public static void thirdCycle() {
+        System.out.println("\nThird cycle \n");
+        int numberCat = 1;
+        do {
+            System.out.println("Cat number " + numberCat + ": Age is " + cat_age + "; Name is " + cat_name);
             numberCat++;
-            System.out.println("Number cat " + numberCat + " Name " + name + " age " + age);
-        }
-    }
-public static void Name(){
-    int length = 10;
-        System.out.println(randomeName(length));
-}
-
-    public static char[] randomeName(int len) {
-        System.out.println("Cat name: ");
-        String charsCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        Random rnd = new Random();
-        char[] name = new char[len];
-
-        for (int i = 0; i < len; i++) {
-            name[i] = charsCaps.charAt(rnd.nextInt(charsCaps.length()));
-        }
-        return name;
-}
-
-
-
-
+        } while ( numberCat <= 10);
     }
 
+    public static void fourhtCycle() {
+        System.out.println("\nFourth cycle \n");
+        Main[] cats = new Main[5];
+        for (int j = 0; j < cats.length; j++) {
+            cats[j] = new Main(Main.randomAge(), Main.randomName());
+        }
+        for (Main cat : cats) {
+            System.out.println(cat.toString());
+        }
+    }
+
+}
 
 
 
--- src/taskhome/taskhome4/Main.java
+++ src/taskhome/taskhome4/Main.java
@@ -1,10 +1,41 @@
 package taskhome.taskhome4;
 
-public class Main {
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.firstCycle();
-        cat.Name();
+import java.util.Random;
 
+public class Main {
+    public int catAge;
+    public String catName;
+
+    public Main(int age, String name) {
+        catAge= age;
+        catName = name;
+    }
+
+    public static void main(String[] args) {
+        Cat.firstCycle();
+        Cat cat = new Cat(randomAge(),randomName());
+        Cat.secondCycle();
+        Cat.thirdCycle();
+        Cat.fourhtCycle();
+    }
+
+    public String toString() {
+        return "Cat age is " + catAge + "\n Cat is name - " + catName;
+    }
+
+    public static String randomName() {
+        String str;
+        String catName = " ";
+        for (int i = 0; i < 5; i++) {
+            Random rand = new Random();
+            str = String.valueOf((char) (rand.nextInt(26) + 'A'));
+            catName = catName + str;
+        }
+        return catName;
+    }
+
+    public static int randomAge() {
+        Random random = new Random();
+        return random.nextInt(27) + 1;
     }
 }
